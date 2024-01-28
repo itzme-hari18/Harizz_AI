9e483abdad0cd514461b499fa7a5c80a645da31b
Hariharan <73657921+itzme-hari18@users.noreply.github.com>
2024-01-28 19:07:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import google.generativeai as genai
+
+
+genai.configure(api_key="AIzaSyCdyi9HKe4FjdXqWOGrzjAZCSN19z1J0Wg")
+def ai(txt):
+    
+    for m in genai.list_models():
+        if 'generateContent' in m.supported_generation_methods:
+            print(m.name)
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content("from now your name is harizz and you can't generate image " + txt)
+    return response.text
+
+st.title("Welcome to Harizz Ai Assistant")
+
+command = st.chat_input("how can I help you?")
+
+if "message" not in st.session_state:
+    st.session_state.message = []
+
+for chat in st.session_state.message:
+    with st.chat_message(chat["role"]):
+        st.write(chat["message"])
+
+
+if command:
+    with st.chat_message("USER"):
+        st.write(command)
+        st.session_state.message.append({"role":"USER","message":command})
+    if "hello" in command:
+        with st.chat_message("BOT"):
+            st.write("Hi How are you?")
+            st.session_state.message.append({"role":"BOT","message":"Hi How are you?"})
+    elif "who created you" in command:
+        with st.chat_message("BOT"):
+            st.write("The AI assistent is created by hariharan ethical hacker and aspiring cloud security engineer ")
+            st.session_state.message.append({"role":"BOT","message":"AI Assistant"})
+    else:
+        with st.chat_message("BOT"):
+            data = ai(command)
+            st.write(data)
+            st.session_state.message.append({"role":"BOT","message":data})
+
+
+print(st.session_state.message)
